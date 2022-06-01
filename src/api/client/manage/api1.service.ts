380fee75b6486793faface60e0f84230ae440281
Kientrung1202 <trungtp1202@gmail.com>
2022-06-01 22:04:52 +0700

    fix: manage get city

--- src/api/client/manage/api1.service.ts
+++ src/api/client/manage/api1.service.ts
@@ -2,25 +2,28 @@ import { Request, Response } from "express";
 import { Area } from "../../../models/area";
 import { badRequest, success } from "../../../utils/response";
 
-export const getListCity = (req: Request, res: Response) => {
-  Area.findAll({
+export const getListCity = async (req: Request, res: Response) => {
+  await Area.findAll({
     attributes: ["belongToProvince"],
     group: "belongToProvince",
-  }).then((results) => {
-    console.log(results, "list city");
-    const data: string[] = [];
-    results.map((result: any) => {
-      const item = result.belongToProvince;
-      data.push(item);
-    });
-    return res.json(success(data));
-  });
+  })
+    .then((results) => {
+      console.log(results, "list city");
+      const data: string[] = [];
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      results.map((result: any) => {
+        const item = result.belongToProvince;
+        data.push(item);
+      });
+      return res.json(success(data));
+    })
+    .catch((err) => res.json(badRequest(err)));
 };
-export const getArea = (req: Request, res: Response) => {
+export const getArea = async (req: Request, res: Response) => {
   const city = req.body.city;
   if (!city) return res.json(badRequest("Missing field: city"));
-  Area.findAll({
-    attributes: ["areaNumber", "areaName", "userId"],
+  await Area.findAll({
+    attributes: ["areaNumber", "areaName"],
     where: {
       belongToProvince: city,
     },
